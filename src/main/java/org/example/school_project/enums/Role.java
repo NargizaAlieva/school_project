21350764b83b5f674b5c4c2d44d265ed3a9b59f5
Alieva Nargiza <nargiza.alieva018@gmail.com>
2024-10-02 14:01:04 +0600

    Create Role enum

---
+++ src/main/java/org/example/school_project/enums/Role.java
@@ -0,0 +1,14 @@
+package org.example.school_project.enums;
+
+public enum Role {
+    ADMIN,
+    USER,
+    SCHOOL_PRINCIPAL,
+    SECRETARY,
+    DEAN,
+    CLASS_TEACHER,
+    TEACHER,
+    CLASS_REPRESENTATIVE,
+    STUDENT,
+    PARENT
+}
